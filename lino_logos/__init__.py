9c0769c8f75c2a5f7327b88e74242896e890eb11
Luc Saffre <luc.saffre@gmail.com>
2014-01-03 03:25:01 +0200

    http://docs.lino-framework.org/blog/2014/0103.html

--- lino_logos/__init__.py
+++ lino_logos/__init__.py
@@ -18,3 +18,4 @@ execfile(os.path.join(os.path.dirname(__file__),'project_info.py'))
 __version__ = SETUP_INFO['version']
 
 intersphinx_url = "http://logos.lino-framework.org"
+srcref_url = 'https://github.com/lsaffre/lino-logos/blob/master/%s'
