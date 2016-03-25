e17846affbe85513bf3bcbb6bb3034cce7fd44bf
Luc Saffre <luc.saffre@gmail.com>
2016-03-25 20:18:35 +0200

    http://luc.lino-framework.org/blog/2016/0325.html

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
-from atelier.tasks import ns, setup_from_tasks
-
-setup_from_tasks(globals(), "lino_logos")
-
-ns.configure({})
+from atelier.tasks import ns
+ns.setup_from_tasks(globals(), "lino_logos")
+# ns.configure({})
 # ns.configure(dict(languages="en de fr et".split()))
