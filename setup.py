64e28d3f6fddbae277cecae29470017199c3c126
Luc Saffre <luc.saffre@gmail.com>
2016-08-03 17:40:12 +0300

    http://luc.lino-framework.org/blog/2016/0803.html

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
-execfile('lino_logos/setup_info.py')
+fn = 'lino_logos/setup_info.py'
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
 if __name__ == '__main__':
     setup(**SETUP_INFO)
