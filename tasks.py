dfbc6c5fc9e74affa502b0e61e093ec49425828b
Luc Saffre <luc.saffre@gmail.com>
2017-03-14 22:27:56 +0200

    http://luc.lino-framework.org/blog/2017/0314.html

--- tasks.py
+++ tasks.py
@@ -1,4 +1,9 @@
 from lino.invlib.ns import ns
-ns.setup_from_tasks(globals(), "lino_logos")
-# ns.configure({})
-# ns.configure(dict(languages="en de fr et".split()))
+ns.setup_from_tasks(
+    globals(), "lino_logos",
+    tolerate_sphinx_warnings=False,
+    blogref_url= 'http://luc.lino-framework.org',
+    revision_control_system='git',
+    cleanable_files=['docs/api/lino_logos.*'],
+    demo_projects=['lino_logos.projects.sacred.settings.demo'])
+
