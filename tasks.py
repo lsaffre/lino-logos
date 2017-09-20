7a1bd81f5681532ae53b7934960058d7436e3c1a
Luc Saffre <luc.saffre@gmail.com>
2017-09-20 15:16:54 +0300

    http://luc.lino-framework.org/blog/2017/0920.html

--- tasks.py
+++ tasks.py
@@ -1,9 +1,9 @@
-from lino.invlib.ns import ns
+from atelier.invlib.ns import ns
 ns.setup_from_tasks(
     globals(), "lino_logos",
     tolerate_sphinx_warnings=False,
     blogref_url= 'http://luc.lino-framework.org',
     revision_control_system='git',
     cleanable_files=['docs/api/lino_logos.*'],
-    demo_projects=['lino_logos.projects.sacred.settings.demo'])
+    demo_projects=['lino_logos/projects/sacred'])
 
