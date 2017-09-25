dbb97fabb083c81591e9fc86b13ba54eb0f11b50
Luc Saffre <luc.saffre@gmail.com>
2017-09-26 00:27:01 +0300

    http://luc.lino-framework.org/blog/2017/0925.html

--- lino_logos/__init__.py
+++ lino_logos/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-## Copyright 2013-2015 Luc Saffre
+## Copyright 2013-2017 Luc Saffre
 # License: BSD (see file COPYING for details)
 """This is the main module of Lino Logos.
 
@@ -15,7 +15,9 @@
 
 import os
 
-execfile(os.path.join(os.path.dirname(__file__),'setup_info.py'))
+fn = os.path.join(os.path.dirname(__file__),'setup_info.py')
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
+
 __version__ = SETUP_INFO['version']
 
 intersphinx_urls = dict(docs="http://logos.lino-framework.org")
