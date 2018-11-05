ba98d0bf7d28d996ceb498de39028ea9b00c0bc6
Luc Saffre <luc.saffre@gmail.com>
2018-11-05 20:01:38 +0200

    http://luc.lino-framework.org/blog/2018/1105.html

--- tasks.py
+++ tasks.py
@@ -5,5 +5,5 @@ ns = setup_from_tasks(
     blogref_url= 'http://luc.lino-framework.org',
     revision_control_system='git',
     cleanable_files=['docs/api/lino_logos.*'],
-    demo_projects=['lino_logos/projects/sacred'])
+    demo_projects=['lino_logos.projects.sacred'])
 
