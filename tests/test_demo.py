ac773784efeecae1f764e8b7c9d28104fbe9c4ee
Luc Saffre <luc.saffre@gmail.com>
2021-03-07 19:54:40 +0200

    do_test_demo_project()

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,17 +1,12 @@
 # how to run a single test:
-# $ python -m unittest tests.test_demo.Main.test_algus1
+# $ python -m unittest tests.test_demo.TestCase.test_sacred
 
 from lino.utils.pythontest import TestCase
 
 
-class Main(TestCase):
+class TestCase(TestCase):
 
-    def do_test_demo_project(self, prjname):
-        """Run :manage:`test` and :manage:`demotest` in a subprocess in the given demo project.
-        """
-        pth = 'lino_logos/projects/' + prjname
-        self.run_django_manage_test(pth)
-        self.run_django_admin_command_cd(pth, "demotest")
+    demo_projects_root = 'lino_logo/projects'
 
-    def test_amici1(self):
+    def test_sacred(self):
         self.do_test_demo_project('sacred')
