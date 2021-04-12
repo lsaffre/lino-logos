22e5b409c071535dd42089d642c849aa492f8ed0
Luc Saffre <luc.saffre@gmail.com>
2021-04-12 22:45:22 +0300

    deactivate build docs

--- lino_logos/__init__.py
+++ lino_logos/__init__.py
@@ -19,6 +19,6 @@ from .setup_info import SETUP_INFO
 
 __version__ = SETUP_INFO['version']
 
-intersphinx_urls = dict(docs="http://logos.lino-framework.org")
-srcref_url = 'https://github.com/lsaffre/lino-logos/blob/master/%s'
-doc_trees = ['docs']
+# intersphinx_urls = dict(docs="http://logos.lino-framework.org")
+# srcref_url = 'https://github.com/lsaffre/lino-logos/blob/master/%s'
+doc_trees = []
